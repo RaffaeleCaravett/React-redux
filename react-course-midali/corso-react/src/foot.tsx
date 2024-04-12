82a78eb49b3430db1a53c2dcdb972e3c3ba6a420
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-04-12 08:30:08 +0200

    added footer and navbar

---
+++ react-course-midali/corso-react/src/foot.tsx
@@ -0,0 +1,22 @@
+const Fot = () =>{
+    return(
+    <footer className="p-2 bg-dark text-white">
+    <div className="row">
+        <div className="col-md-3">
+            <h1>Ciao</h1>
+        </div>
+        <div className="col-md-3">
+            <h1>Ciao</h1>
+        </div>
+        <div className="col-md-3">
+            <h1>Ciao</h1>
+        </div>
+        <div className="col-md-3">
+            <h1>Ciao</h1>
+        </div>
+    </div>
+</footer>
+)
+}
+
+export default Fot;
---
+++ react-course-midali/corso-react/src/nav.tsx
@@ -0,0 +1,23 @@
+const Nav = () =>{
+    return(
+<nav className="p-2 bg-dark text-white">
+    <div className="row">
+        <div className="col-md-3">
+            <h1>Ciao</h1>
+        </div>
+        <div className="col-md-3">
+            <h1>Ciao</h1>
+        </div>
+        <div className="col-md-3">
+            <h1>Ciao</h1>
+        </div>
+        <div className="col-md-3">
+            <h1>Ciao</h1>
+        </div>
+    </div>
+</nav>
+
+    )
+}
+
+export default Nav;
