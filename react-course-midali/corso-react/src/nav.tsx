7fa540dbfd7c89d99a6417012f34bd43be8eeeed
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-04-13 16:12:53 +0200

    deleted Link.tsx

--- react-course-midali/corso-react/src/nav.tsx
+++ react-course-midali/corso-react/src/nav.tsx
@@ -1,20 +1,15 @@
-import Links from './Link.tsx'
 
 const Nav = () =>{
     return(
 <nav className="p-2 bg-dark text-white">
     <div className="row">
         <div className="col-md-3">
-            <Links  param="home" />
         </div>
         <div className="col-md-3">
-            <Links  param='blogs'/>
         </div>
         <div className="col-md-3">
-            <Links  param='fun'/>
         </div>
         <div className="col-md-3">
-            <Links  param='login'/>
         </div>
     </div>
 </nav>
