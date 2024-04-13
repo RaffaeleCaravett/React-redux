9dfd892cc29193847f3e22e55d019486dc31f3a9
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-04-13 21:17:43 +0200

    doin form

--- react-course-midali/corso-react/src/Login.tsx
+++ react-course-midali/corso-react/src/Login.tsx
@@ -1,8 +1,25 @@
 const Login = () =>{
     return (
-<div>
-    Hihih
-</div>
+        <div className="row m-0 p-0 background">
+          <div className="container py-5 text-center">
+  <div className="row py-5">
+    <div className="col-md-6">
+        <form action="" className="border p-3 rounded shadow">
+    <label htmlFor=""></label>
+    <input type="email" required className="form-control" />
+    <label htmlFor=""></label>
+    <input type="password" required className="form-control" />
+    <button className="btn m-1"> Login</button>
+</form>
+    </div>
+    <div className="col-md-6">
+<h1>Your absolute wonderful website</h1>
+    </div>
+
+  </div>
+</div>  
+        </div>
+
 
 
     )
