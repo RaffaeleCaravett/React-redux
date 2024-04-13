61335f859c5ec071a722f4b1556e32161b30224c
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-04-13 21:30:26 +0200

    done stuff for login

--- react-course-midali/corso-react/src/Login.tsx
+++ react-course-midali/corso-react/src/Login.tsx
@@ -1,20 +1,32 @@
+import { useState } from "react";
+
 const Login = () =>{
+
+const [email,setEmail] = useState('')
+const [password,setPassword] = useState('')
+
+
+
+
+
     return (
         <div className="row m-0 p-0 background">
           <div className="container py-5 text-center">
   <div className="row py-5">
-    <div className="col-md-6">
-        <form action="" className="border p-3 rounded shadow">
-    <label htmlFor=""></label>
-    <input type="email" required className="form-control" />
-    <label htmlFor=""></label>
-    <input type="password" required className="form-control" />
-    <button className="btn m-1"> Login</button>
+     <div className="col-md-6 py-5">
+<h1 className="py-5">Your absolute wonderful website</h1>
+    </div>
+    <div className="col-md-6 py-5">
+        <form action="" className="border p-3 rounded shadow py-5">
+            <h1>Accedi</h1>
+    <label className="py-2">Inserisci l'email</label>
+    <input type="email" required className="form-control w-75 m-auto" onChange={(e)=>setEmail(e.target.value)}/>
+    <label className="py-2">Inserisci la password</label>
+    <input type="password" required className="form-control w-75 m-auto" onChange={(e)=>setPassword(e.target.value)}/>
+    <button className="btn m-1" onClick={()=>console.log(email,password)}> Login</button>
 </form>
     </div>
-    <div className="col-md-6">
-<h1>Your absolute wonderful website</h1>
-    </div>
+   
 
   </div>
 </div>  
