3199f7f66d88a31206eff2ac4e8e1ea09a7554bc
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-04-14 11:56:34 +0200

    created user class

--- react-course-midali/blogs/src/main/java/com/example/blogs/user/User.java
+++ react-course-midali/blogs/src/main/java/com/example/blogs/user/User.java
@@ -3,14 +3,55 @@ package com.example.blogs.user;
 import com.example.blogs.enums.Role;
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
 
 @Entity
 @Table(name = "users")
 @Data
-public class User {
+public class User implements UserDetails {
 @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 @Enumerated(EnumType.STRING)
 private Role role;
+private String nome;
+private String cognome;
+private String email;
+private String password;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority(this.role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
