1564f29fae12f0f44f0d0ace19ff2e3f28f1caaa
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-04-14 15:46:55 +0200

    done userRepository

--- react-course-midali/blogs/src/main/java/com/example/blogs/user/UserRepository.java
+++ react-course-midali/blogs/src/main/java/com/example/blogs/user/UserRepository.java
@@ -1,4 +1,12 @@
 package com.example.blogs.user;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserRepository extends JpaRepository<Long,User> {
+
+    List<User> findByEmailContaining(String Email);
 }
--- react-course-midali/blogs/src/main/java/com/example/blogs/user/UserService.java
+++ react-course-midali/blogs/src/main/java/com/example/blogs/user/UserService.java
@@ -1,4 +1,7 @@
 package com.example.blogs.user;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
 }
