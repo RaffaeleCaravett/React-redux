3ac0bc428b64ec3e892ec0c6449c0f7d3cb93d88
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-04-14 21:27:13 +0200

    created blogDTO

---
+++ react-course-midali/blogs/src/main/java/com/example/blogs/blog/BlogService.java
@@ -0,0 +1,13 @@
+package com.example.blogs.blog;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BlogService {
+@Autowired
+    BlogRepository blogRepository;
+
+
+public Blog save()
+}
---
+++ react-course-midali/blogs/src/main/java/com/example/blogs/payloads/entities/BlogDTO.java
@@ -0,0 +1,18 @@
+package com.example.blogs.payloads.entities;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+public record BlogDTO(
+        @NotEmpty(message = "Text is empty")
+        String testo,
+        @NotEmpty(message = "Categoria is empty")
+        String categoria,
+         @NotEmpty(message = "Missing author")
+        String autore,
+        @NotEmpty(message = "Title is empty")
+        String titolo,
+        @NotNull(message = "Reading time is empty")
+        int tempoLettura
+) {
+}
