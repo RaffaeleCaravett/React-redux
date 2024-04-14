bf4988dd4d139344d07dd9e83140177a090d8257
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-04-14 11:50:26 +0200

    created Role enum

--- react-course-midali/blogs/src/main/java/com/example/blogs/user/User.java
+++ react-course-midali/blogs/src/main/java/com/example/blogs/user/User.java
@@ -1,4 +1,16 @@
 package com.example.blogs.user;
 
+import com.example.blogs.enums.Role;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
 public class User {
+@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+@Enumerated(EnumType.STRING)
+private Role role;
 }
