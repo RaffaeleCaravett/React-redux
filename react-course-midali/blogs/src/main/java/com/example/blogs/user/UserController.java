d1c70077022a05cd4311bec18954a5b943550c4d
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-04-14 16:10:25 +0200

    created userController

--- react-course-midali/blogs/src/main/java/com/example/blogs/user/UserController.java
+++ react-course-midali/blogs/src/main/java/com/example/blogs/user/UserController.java
@@ -1,4 +1,48 @@
 package com.example.blogs.user;
 
+import com.example.blogs.payloads.entities.UserRegistrationDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @PutMapping("/{id}")
+    @PreAuthorize("HasAuthority('Admin')")
+    public User putById(@PathVariable long id, @RequestBody UserRegistrationDTO userRegistrationDTO){
+        return userService.updateById(id,userRegistrationDTO);
+    }
+    @PutMapping("/me")
+    @PreAuthorize("HasAuthority('User')")
+    public User putById(@AuthenticationPrincipal User currentUser, @RequestBody UserRegistrationDTO userRegistrationDTO){
+        return userService.updateById(currentUser.getId(),userRegistrationDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("HasAuthority('Admin')")
+    public Boolean deleteById(@PathVariable long id){
+        return userService.deleteById(id);
+    }
+    @DeleteMapping("/me")
+    @PreAuthorize("HasAuthority('User')")
+    public Boolean deleteById(@AuthenticationPrincipal User currentUser, @RequestBody UserRegistrationDTO userRegistrationDTO){
+        return userService.deleteById(currentUser.getId());
+    }
+
+    @GetMapping("/{id}")
+    public User getById(long id){
+        return userService.getById(id);
+    }
+    @GetMapping("/{email}")
+    public List<User> findByEmailContaining(@PathVariable String email){
+        return userService.findByEmailContaining(email);
+    }
 }
