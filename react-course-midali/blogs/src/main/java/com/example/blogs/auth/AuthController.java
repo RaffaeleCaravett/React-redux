a40e9428a66dd7cfeab02ce25c0225be0b6eaa8d
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-04-15 12:02:23 +0200

    done save user in auth service and controller

--- react-course-midali/blogs/src/main/java/com/example/blogs/auth/AuthController.java
+++ react-course-midali/blogs/src/main/java/com/example/blogs/auth/AuthController.java
@@ -1,4 +1,41 @@
 package com.example.blogs.auth;
 
+
+import com.example.blogs.enums.Role;
+import com.example.blogs.exceptions.BadRequestException;
+import com.example.blogs.payloads.entities.UserRegistrationDTO;
+import com.example.blogs.user.User;
+import com.example.blogs.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.convert.ReadingConverter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
+
+@Autowired
+    AuthService authService;
+@Autowired
+    UserRepository userRepository;
+@Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+
+@PostMapping("")
+public User save(@RequestBody @Validated UserRegistrationDTO userRegistrationDTO, BindingResult validation){
+    if(userRepository.findByEmail(userRegistrationDTO.email()).isPresent()){
+        throw new BadRequestException("User con email " + userRegistrationDTO.email() + " già presente in db.");
+    }
+
+  return authService.save(userRegistrationDTO);
+
+    }
+
+
 }
--- react-course-midali/blogs/src/main/java/com/example/blogs/auth/AuthService.java
+++ react-course-midali/blogs/src/main/java/com/example/blogs/auth/AuthService.java
@@ -1,4 +1,33 @@
 package com.example.blogs.auth;
 
+import com.example.blogs.enums.Role;
+import com.example.blogs.payloads.entities.UserRegistrationDTO;
+import com.example.blogs.user.User;
+import com.example.blogs.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AuthService {
+
+@Autowired
+    UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder bcrypt;
+
+
+    public User save(UserRegistrationDTO userRegistrationDTO){
+        User user = new User();
+        user.setCognome(userRegistrationDTO.cognome());
+        user.setNome(userRegistrationDTO.nome());
+        user.setEmail(userRegistrationDTO.email());
+        if(userRegistrationDTO.email().equals("raffaele.caravetta13@gmail.com")) {
+            user.setRole(Role.Admin);
+        }else {
+            user.setRole(Role.User);
+        }
+        user.setPassword(bcrypt.encode(userRegistrationDTO.password()));
+        return userRepository.save(user);
+    }
 }
