9b8c6b82ad0cab5bc652db3aa1c9fcf53a4cf1f9
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-04-16 14:33:13 +0200

    doing login component

--- react-course-midali/angular-blogs-midali/src/app/components/login/login.component.ts
+++ react-course-midali/angular-blogs-midali/src/app/components/login/login.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
 
+  param:string=''
+
+  constructor(private activatedRoute:ActivatedRoute){
+    this.activatedRoute.params.subscribe(params => {
+      this.param=params['param']
+    })
+  }
+
+  ngOnInit():void{
+
+  }
 }
