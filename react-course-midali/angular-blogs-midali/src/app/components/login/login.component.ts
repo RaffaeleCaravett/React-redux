fda5a1ad1832192cf0083a0de585b48d0141fcc3
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-04-16 21:40:16 +0200

    setted background for login component

--- react-course-midali/angular-blogs-midali/src/app/components/login/login.component.ts
+++ react-course-midali/angular-blogs-midali/src/app/components/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -20,7 +20,17 @@ export class LoginComponent implements OnInit{
   }
 
   ngOnInit():void{
-
+    this.form= new FormGroup({
+      email:new FormControl('',[Validators.required,Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
+      password:new FormControl('',[Validators.required,Validators.minLength(6)])
+    })
+    this.formSignup=new FormGroup({
+      nome: new FormControl('',[Validators.required,Validators.minLength(2)]),
+      cognome: new FormControl('',[Validators.required,Validators.minLength(2)]),
+      email:new FormControl('',[Validators.required,Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
+    password:new FormControl('',[Validators.required,Validators.minLength(6)]),
+    ripeti:new FormControl('',[Validators.required,Validators.minLength(6)])
+    })
   }
 
   login():void{
