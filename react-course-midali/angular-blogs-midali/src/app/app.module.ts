7d4f9bb4ee9147630e0c54641885e0add825c544
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-04-16 11:44:51 +0200

    created login component

--- react-course-midali/angular-blogs-midali/src/app/app.module.ts
+++ react-course-midali/angular-blogs-midali/src/app/app.module.ts
@@ -4,10 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent,
+    HomeComponent,
+    LoginComponent
+
   ],
   imports: [
     BrowserModule,
