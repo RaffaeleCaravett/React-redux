3d29ba98663313ac06ff08fbac8442cd72175f2c
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-04-20 17:04:03 +0200

    succesfuly retrieving user in blog component

--- react-course-midali/angular-blogs-midali/src/app/components/blogs/blogs.component.ts
+++ react-course-midali/angular-blogs-midali/src/app/components/blogs/blogs.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-blogs',
   templateUrl: './blogs.component.html',
   styleUrls: ['./blogs.component.scss']
 })
-export class BlogsComponent {
+export class BlogsComponent implements OnInit{
+user:any
+constructor(private authService:AuthService,private router:Router){}
 
+ngOnInit(): void {
+  this.authService.verifyAccessToken(localStorage.getItem('accessToken')!).subscribe({
+    next:(success:any)=>{
+    this.user=success
+    console.log(this.user)
+    this.authService.token=localStorage.getItem('accessToken')!
+    this.authService.authenticateUser(true)
+      this.router.navigate(['/blog'])
+    },
+    error:(err:any)=>{
+this.authService.authenticateUser(false)
+this.authService.token=''
+this.authService.refreshToken=''
+localStorage.clear()
+this.router.navigate([''])
+    },
+    complete:()=>{}
+})
+
+}
 }
