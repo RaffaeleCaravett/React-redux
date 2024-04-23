71cde91be9947dab2f892534949751ff833a5a77
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-04-23 15:08:27 +0200

    doing now modify dialog

--- react-course-midali/angular-blogs-midali/src/app/components/show-blog/show-blog.component.ts
+++ react-course-midali/angular-blogs-midali/src/app/components/show-blog/show-blog.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-show-blog',
   templateUrl: './show-blog.component.html',
   styleUrls: ['./show-blog.component.scss']
 })
-export class ShowBlogComponent {
+export class ShowBlogComponent implements OnInit{
 
+blog:any
+constructor(@Inject(MAT_DIALOG_DATA) public dialogData:any ,public dialogRef: MatDialogRef<ShowBlogComponent>,) { }
+
+ngOnInit():void{
+this.blog=this.dialogData
+}
 }
