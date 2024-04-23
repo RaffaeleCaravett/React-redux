07cd82a5b53c92b69afc451029eec1796f92a131
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-04-23 15:34:46 +0200

    modify and delete done succesfuly

--- react-course-midali/angular-blogs-midali/src/app/components/show-blog/show-blog.component.ts
+++ react-course-midali/angular-blogs-midali/src/app/components/show-blog/show-blog.component.ts
@@ -1,5 +1,8 @@
 import { Component, Inject, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { BlogDTO } from 'src/app/core/interfaces';
+import { BlogService } from 'src/app/services/blog.service';
 
 @Component({
   selector: 'app-show-blog',
@@ -9,9 +12,79 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 export class ShowBlogComponent implements OnInit{
 
 blog:any
-constructor(@Inject(MAT_DIALOG_DATA) public dialogData:any ,public dialogRef: MatDialogRef<ShowBlogComponent>,) { }
+insertBlogForm!:FormGroup
+categories:any[]=[]
+insertBlogError:string=""
+insertBlogSubmitted:boolean=false
+showSuccessMessage:string=''
+constructor(@Inject(MAT_DIALOG_DATA) public dialogData:any ,public dialogRef: MatDialogRef<ShowBlogComponent>,private blogService:BlogService) { }
 
 ngOnInit():void{
 this.blog=this.dialogData
+this.blogService.getAllCategories().subscribe({
+  next:(categories:any)=>{
+    this.categories=categories
+    },
+    error:(err:any)=>{
+this.categories=[]
+    },
+    complete:()=>{}
+})
+this.insertBlogForm= new FormGroup({
+  titolo:new FormControl(this.blog.titolo,Validators.required),
+  categoria:new FormControl(this.blog.categoria,Validators.required),
+  testo:new FormControl(this.blog.testo,Validators.required),
+  autore:new FormControl(this.blog.autore,Validators.required),
+  tempoLettura:new FormControl(this.blog.tempoLettura,[Validators.required,Validators.min(1)]),
+})
 }
+
+putBlog(){
+  this.insertBlogError=""
+  this.insertBlogSubmitted=true
+if(this.insertBlogForm.valid){
+let blog:BlogDTO={
+  titolo:this.insertBlogForm.controls['titolo'].value,
+  testo:this.insertBlogForm.controls['testo'].value,
+  autore:this.insertBlogForm.controls['autore'].value,
+  categoria:this.insertBlogForm.controls['categoria'].value,
+  user_id:this.blog.user.id,
+  tempoLettura:this.insertBlogForm.controls['tempoLettura'].value
+}
+this.blogService.put(this.blog.id,blog).subscribe(
+  {
+    next:(blog:any)=>{
+
+      this.insertBlogError=""
+     this.insertBlogSubmitted=false
+     this.showSuccessMessage='Blog modificato con successo.'
+      },
+      error:(err:any)=>{
+  this.insertBlogError=err.error.message
+      },
+      complete:()=>{}
+  }
+)
+}else{
+  this.insertBlogError="Compila tutto il form"
+}
+}
+
+deleteBlog(){
+  this.blogService.delete(this.blog.id).subscribe({
+    next:(blog:any)=>{
+      if(blog){
+      this.insertBlogForm.reset()
+      this.insertBlogError=""
+      this.insertBlogSubmitted=false
+      this.showSuccessMessage='Blog eliminato con successo.'
+    }
+  },
+       error:(err:any)=>{
+   this.insertBlogError=err.error.message
+       },
+       complete:()=>{}
+  })
+}
+
 }
