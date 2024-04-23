d74958158b541196fb5a51d874e7cc0f2370358b
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-04-23 20:17:34 +0200

    now doing login

--- react-course-midali/corso-react/src/Login.tsx
+++ react-course-midali/corso-react/src/Login.tsx
@@ -6,6 +6,21 @@ const [email,setEmail] = useState('')
 const [password,setPassword] = useState('')
 
 
+const login = () =>{
+    if (/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(email)) { 
+const body = 
+{
+    email:email,
+    password:password
+}
+document.getElementsByClassName('email')[0].textContent=""
+document.getElementsByClassName('password')[0].textContent=""
+console.log(body)
+    }else{
+document.getElementsByClassName('email')[0].textContent="Campo invalido. Prova una email tipo 'a@a.com'"
+    }
+
+}
 
 
 
@@ -17,13 +32,15 @@ const [password,setPassword] = useState('')
 <h1 className="py-5">Your absolute wonderful website</h1>
     </div>
     <div className="col-md-6 py-5">
-        <form action="" className="border p-3 rounded shadow py-5">
+        <form className="border p-3 rounded shadow py-5 bg">
             <h1>Accedi</h1>
     <label className="py-2">Inserisci l'email</label>
     <input type="email" required className="form-control w-75 m-auto" onChange={(e)=>setEmail(e.target.value)}/>
+    <p className="text-danger email"></p>
     <label className="py-2">Inserisci la password</label>
     <input type="password" required className="form-control w-75 m-auto" onChange={(e)=>setPassword(e.target.value)}/>
-    <button className="btn m-1" onClick={()=>console.log(email,password)}> Login</button>
+    <p className="text-danger password"></p>
+    <button className="btn m-3" type="button" onClick={()=>login()}> Login</button>
 </form>
     </div>
    
