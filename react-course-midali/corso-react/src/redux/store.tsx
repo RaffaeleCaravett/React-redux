542486d5b3273f8ae3b5c66705f160c3c92b45a7
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-04-28 15:17:59 +0200

    disabled some typescript default options

---
+++ react-course-midali/corso-react/src/redux/store.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable react-refresh/only-export-components */
+import { configureStore } from "@reduxjs/toolkit";
+import { userReducer } from "./userSlice";
+import { accessTokenReducer } from "./accessTokenSlice";
+
+export default configureStore({
+    reducer: {
+accessToken : accessTokenReducer,
+user: userReducer
+    }
+})
