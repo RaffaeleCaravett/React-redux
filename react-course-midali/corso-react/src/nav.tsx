efae43aba363dd91b4a90bdf4fd38764931ccf6e
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-04-29 16:44:14 +0200

    modified navbar

--- react-course-midali/corso-react/src/nav.tsx
+++ react-course-midali/corso-react/src/nav.tsx
@@ -1,12 +1,20 @@
-import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
 import { tokenInterface } from "./interfaces/interfaces";
+import { setAccessToken, setIsLoggedIn } from "./redux/accessTokenSlice";
+import { setUser } from "./redux/userSlice";
 
 const Nav = () =>{
-
-
+    const dispatch = useDispatch();
+const navigate = useNavigate();
 const isLoggedIn = useSelector((state:tokenInterface)=>state.accessToken.isLoggedIn)
-
+const logout = () => {
+    localStorage.clear()
+  dispatch(setIsLoggedIn(false));
+  dispatch(setUser(''))
+  dispatch(setAccessToken(''))
+navigate('/home')
+} 
     return(
 <nav className="p-2 bg-yellow-500 text-dark text-center">
     <div className="row">
@@ -17,7 +25,8 @@ const isLoggedIn = useSelector((state:tokenInterface)=>state.accessToken.isLogge
             {isLoggedIn && <Link to={'/Blog'}>Blog</Link>} 
         </div>
         <div className="col-md-4 p-2">
-            <Link to={'/Login'}>Login</Link>
+        {isLoggedIn && <button className="btn" onClick={()=>{logout()}}>Logout</button> }
+        {!isLoggedIn &&<Link to={'/Login'}>Login</Link>}
         </div>
     </div>
 </nav>
