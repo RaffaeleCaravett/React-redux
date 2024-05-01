a52eedf89fa3516ff2c167ab0dd82f4eca33dbc6
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-05-01 21:42:00 +0200

    displaying all blog's datas

--- react-course-midali/corso-react/src/singleBlogComponent.tsx
+++ react-course-midali/corso-react/src/singleBlogComponent.tsx
@@ -11,6 +11,13 @@ const blogs = props.blogs;
     <div className="col-md-3 p-2" key={b.id}>
         <div className="p-1">
             <h2>{b.titolo}</h2>
+            <p className="fs-6">{b.categoria + " " + b.tempoLettura}</p>
+        </div>
+        <div className="p-1">
+            {b.testo}
+        </div>
+        <div className="p-1">
+            {b.autore}
         </div>
     </div>
   ))  
